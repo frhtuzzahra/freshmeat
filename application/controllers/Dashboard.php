1a23bcc4a43746dce98700cc93884038429c1aaa
Taufik Ridho <taufikridho505@gmail.com>
2023-07-27 16:05:14 +0700

    add landing

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,14 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
-	
+class Dashboard extends CI_Controller
+{
+
 	public function index()
 	{
-		if ($this->session->userdata('status') == 'login' ) {
+		if ($this->session->userdata('status') == 'login') {
 			$this->load->view('dashboard');
 		} else {
-			$this->load->view('login');
+			// $this->load->view('login');
+			$this->load->view('landing');
 		}
 	}
 }
